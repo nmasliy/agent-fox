5dda177309c87ffe67a2538488ba213f364662cf
n1colashka <deckards2505@gmail.com>
2023-01-13 09:01:18 +0600

    add hero, projects sections

--- src/js/_components.js
+++ src/js/_components.js
@@ -4,3 +4,5 @@ import './components/tabs';
 import './components/accordion';
 import './components/modals';
 import './components/sliders';
+// import './components/fullpage-scroll';
+// import './components/fix-full-height';
--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,5 +1,16 @@
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// Swiper.use([Navigation, Pagination]);
-// const swiper = new Swiper(el, {
-//   slidesPerView: 'auto',
-// });
+import Swiper, { Navigation, Pagination, Mousewheel } from 'swiper';
+
+Swiper.use([Navigation, Pagination, Mousewheel]);
+
+const swiper = new Swiper('.projects__slider', {
+  speed: 500,
+  navigation: {
+    prevEl: '.projects__prev',
+    nextEl: '.projects__next',
+  },
+  mousewheel: {
+    invert: true,
+    eventsTarget: '.projects',
+    releaseOnEdges: true,
+  },
+});
